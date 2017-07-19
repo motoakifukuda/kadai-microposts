be82b4586549a977f4d8cb025afde48d2a2496ea
MotoakiFukuda <motoaki.fukuda@bravi.co.jp>
2017-07-19 08:39:59 +0000

    20170719-2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,6 +36,9 @@ Route::group(['middleware' => 'auth'], function () {
         Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
         Route::get('followings', 'UsersController@followings')->name('users.followings');
         Route::get('followers', 'UsersController@followers')->name('users.followers');
+        Route::post('favorite', 'MicropostFavoriteController@store')->name('user.favorite');
+        Route::delete('unfavorite', 'MicropostFavoriteController@destroy')->name('user.unfavorite');
+        Route::get('favoritings', 'UsersController@favoritings')->name('users.favoritings');
     });
     
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -19,6 +19,9 @@
                     {!! Form::close() !!}
                 @endif
             </div>
+            <div>
+                @include('favorite.favorite_button', ['user' => $user])
+            </div>
         </div>
     </li>
 @endforeach
